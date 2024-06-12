2caed24d8b984e86507f538d612d6615094eae6a
anthonyshibo <133836502+anthonyshibo@users.noreply.github.com>
2024-06-13 01:29:01 +0800

    Update PlaygroundHeader.tsx

--- src/components/playground/PlaygroundHeader.tsx
+++ src/components/playground/PlaygroundHeader.tsx
@@ -75,40 +75,20 @@ export const PlaygroundHeader = ({
 
 const LKLogo = () => (
   <svg
-    width="28"
-    height="28"
-    viewBox="0 0 32 32"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    fill="currentColor"
+    className="bi bi-soundwave"
+    viewBox="0 0 16 16"
   >
-    <g clipPath="url(#clip0_101_119699)">
-      <path
-        d="M19.2006 12.7998H12.7996V19.2008H19.2006V12.7998Z"
-        fill="currentColor"
-      />
-      <path
-        d="M25.6014 6.40137H19.2004V12.8024H25.6014V6.40137Z"
-        fill="currentColor"
-      />
-      <path
-        d="M25.6014 19.2002H19.2004V25.6012H25.6014V19.2002Z"
-        fill="currentColor"
-      />
-      <path d="M32 0H25.599V6.401H32V0Z" fill="currentColor" />
-      <path d="M32 25.5986H25.599V31.9996H32V25.5986Z" fill="currentColor" />
-      <path
-        d="M6.401 25.599V19.2005V12.7995V6.401V0H0V6.401V12.7995V19.2005V25.599V32H6.401H12.7995H19.2005V25.599H12.7995H6.401Z"
-        fill="white"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_101_119699">
-        <rect width="32" height="32" fill="white" />
-      </clipPath>
-    </defs>
+    <path
+      fillRule="evenodd"
+      d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5zm-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5zm12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5z"
+    />
   </svg>
 );
-
+export default LKLogo;
 const GithubSVG = () => (
   <svg
     width="24"
